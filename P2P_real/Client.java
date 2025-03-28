266c5fd13eeab94568a6fedf5560f9ce497789f8
davs22 <mdiirr2003@gmail.com>
2025-03-28 04:54:47 -0300

    Adicionado diretório P2P_real

--- P2P_real/Client.java
+++ P2P_real/Client.java
@@ -1,6 +1,8 @@
 package P2P_real;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[]args){
@@ -8,6 +10,14 @@ public class Client {
         try {
             
             Socket socket = new Socket("localhost", 8081);
+            Scanner scanner = new Scanner(System.in);
+            
+            ClientThread clientThread = new ClientThread(socket);
+            clientThread.start();
+
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            String teclado = scanner.nextLine();
+            saida.println(teclado);
 
         } catch (Exception e) {
            
--- P2P_real/ClientThread.java
+++ P2P_real/ClientThread.java
@@ -1,29 +1,33 @@
 package P2P_real;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
-    public static void main(String[]args) throws IOException{
+public class ClientThread extends Thread {
+
+    private Socket socket;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
         try {
             
-       ServerSocket serverSocket = new ServerSocket(8081);
-        Socket socket = serverSocket.accept();
-        System.out.println("Entrou");
-
         InputStreamReader Streamreader = new InputStreamReader(socket.getInputStream());
-        PrintStream saida = new PrintStream(socket.getOutputStream());
         BufferedReader BufferedReader = new BufferedReader(Streamreader);
 
         String x;
         while ((x = BufferedReader.readLine()) != null) {
-            saida.println ("Mensagem recebida: " +x); 
+            System.out.println("Cliente: " + x);
         }
-    } catch (Exception e) {
-       
-    }
+        } catch (Exception e) {
+            
+        }
+           
     }
 }
