245e76ef01ebb7bdf9f29041abd64a5e2041b4e7
okanjota <jaimeejunioorr@gmail.com>
2025-03-31 20:48:43 -0300

    sout added

---
+++ ThreadEnviar.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ThreadEnviar implements Runnable {
+    private String ip;
+    private int portadestino;
+
+    public ThreadEnviar(String ip, int portadestino) {
+        this.ip = ip;
+        this.portadestino = portadestino;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket(ip, portadestino);
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("caso deseje sair digite 'chat_close'");
+            
+            while (true) {
+                String sms = scanner.nextLine();
+                output.println(sms);
+
+                if (sms.equalsIgnoreCase("chat_close")) {
+                    break;
+                }
+
+            }
+            socket.close();
+            scanner.close();
+
+        } catch (IOException e) { // Mudança para IOException
+            System.out.println("Erro ao conectar ou enviar mensagem: " + e.getMessage());
+        }
+
+    }
+}
