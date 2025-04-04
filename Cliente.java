2bcf462c1488bf789bb2c5f77e33e27c77e4d452
victorcaetano2025 <victorcaetano2711@gmail>
2025-04-04 16:33:09 -0300

    ajustes pre final

--- Cliente.java
+++ Cliente.java
@@ -8,36 +8,37 @@ import java.util.Scanner; //permite a leitura do teclado
 
 public class Cliente {
     public static void main(String[] args) {
-        //"try" para fechar tudo apos o uso
-        try (Scanner scanner = new Scanner(System.in)) { //cria um Scanner
+        // "try" para fechar tudo apos o uso
+        try (Scanner scanner = new Scanner(System.in)) { // cria um Scanner
             System.out.println("Digite sua porta: ");
-            int minhaPorta = Integer.parseInt(scanner.nextLine()); //pega uma String e converte em Inteiro guardando na variavel minhaPorta
-           
-            Thread threadSever = new Thread(new Serveropener(minhaPorta)); //cria uma nova Thread com um parametro
-            threadSever.start(); //inicia a Thread
-            System.out.println("Servidor iniciado na porta "+ minhaPorta);
+            int minhaPorta = Integer.parseInt(scanner.nextLine()); // pega uma String e converte em Inteiro guardando na
+                                                                   // variavel minhaPorta
+
+            Thread threadSever = new Thread(new Serveropener(minhaPorta)); // cria uma nova Thread com um parametro
+            threadSever.start(); // inicia a Thread
+            System.out.println("Servidor iniciado na porta " + minhaPorta);
 
             System.out.println("Digite o seu IP (caso seja na mesma maquina digite localhost): ");
             String meuIp = scanner.nextLine();
 
             System.out.println("Digite agora a porta que deseja conectar: ");
             int portaDestino = Integer.parseInt(scanner.nextLine());
-            
+
             System.out.println("Tentando conectar ao IP " + meuIp + " na porta " + portaDestino + "...");
-            
-            Socket socket = new Socket(meuIp, portaDestino); //cria um novo socket com parametros
+
+            Socket socket = new Socket(meuIp, portaDestino); // cria um novo socket com parametros
             System.out.println("Conexão estabelecida com sucesso!");
 
-            Thread threadEnvio = new Thread(new ThreadEnviar(socket)); //cria thread de enviar
-            threadEnvio.start(); //inicia thread de enviar
+            Thread threadEnvio = new Thread(new ThreadEnviar(socket)); // cria thread de enviar
+            threadEnvio.start(); // inicia thread de enviar
 
-            threadEnvio.join(); //faz esperar a thread atual terminar de rodar
+            threadEnvio.join(); // faz esperar a thread atual terminar de rodar
 
-        } catch (NumberFormatException e) { //caso seja digitado algo que nao seja numero na porta
+        } catch (NumberFormatException e) { // caso seja digitado algo que nao seja numero na porta
             System.err.println("Erro: Digite um número válido para a porta.");
-        } catch (IOException e) { //caso alguma thread ou socket falhem
+        } catch (IOException e) { // caso alguma thread ou socket falhem
             System.err.println("Erro ao conectar: " + e.getMessage());
-        } catch (InterruptedException e) { //caso algo seja interrompido
+        } catch (InterruptedException e) { // caso algo seja interrompido
             System.err.println("Erro ao aguardar thread: " + e.getMessage());
         }
 
--- Serveropener.java
+++ Serveropener.java
@@ -2,29 +2,27 @@ import java.io.IOException; //trata execoes de entrada e saida
 import java.net.ServerSocket; //permite utilizar serverSocket
 import java.net.Socket; //permite o uso de socket
 
-class Serveropener implements Runnable { //implementa numa Thread
+class Serveropener implements Runnable { // implementa numa Thread
 
-    private int porta; //cria uma variavel porta
+    private int porta; // cria uma variavel porta
 
-    public Serveropener(int porta) { //instancia a classe
+    public Serveropener(int porta) { // instancia a classe
         this.porta = porta;
 
     }
 
-    @Override //ajuda a avisar erros e sobrescrever metodos
-    public void run() { //chama a thread
-        //"try" para fechar tudo apos o uso e tratar execoes
-        try (ServerSocket server = new ServerSocket(porta)) { //cria um novo server
+    @Override // ajuda a avisar erros e sobrescrever metodos
+    public void run() { // chama a thread
+        // "try" para fechar tudo apos o uso e tratar execoes
+        try (ServerSocket server = new ServerSocket(porta)) { // cria um novo server
             System.out.println("aguardando alguem se conectar na porta: " + porta);
             while (true) {
-                Socket conectou = server.accept(); //bloqueia ate se conectar
-                String ipsocket = conectou.getInetAddress().getHostAddress(); //permite conexao e retorna o ip
-                System.out.println("Cliente conectado: " + ipsocket );
+                Socket conectou = server.accept(); // bloqueia ate se conectar
 
-                new Thread(new Receptor(conectou)).start(); //permite que continue acietando conexoes
+                new Thread(new Receptor(conectou)).start(); // permite que continue acietando conexoes
             }
-        } catch (IOException e) { //caso alguma thread ou socket falhem
-            System.err.println("Erro no servidor: "+e.getMessage());
+        } catch (IOException e) { // caso alguma thread ou socket falhem
+            System.err.println("Erro no servidor: " + e.getMessage());
 
         }
     }
